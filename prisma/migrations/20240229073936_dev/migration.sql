493c965f06acd45ba73234177feba8e336885dd8
nadriha <hafez.nadhira@gmail.com>
2024-02-29 15:01:07 +0700

    init db

---
+++ prisma/migrations/20240229073936_dev/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "balance" DECIMAL NOT NULL,
+    "expences" DECIMAL NOT NULL,
+    "income" DECIMAL NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Transactions" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "createDate" DATETIME NOT NULL,
+    "amount" DECIMAL NOT NULL,
+    "description" TEXT,
+    "paymentType" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    CONSTRAINT "Transactions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
